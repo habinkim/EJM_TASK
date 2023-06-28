606de93582b9e7318b664f4554c1a0fec56de295
habin <danny9643@naver.com>
2023-06-28 18:33:38 +0900

    first commit

---
+++ src/main/java/kr/wooriga/ejm_task/EjmTaskApplication.java
@@ -0,0 +1,13 @@
+package kr.wooriga.ejm_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjmTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjmTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/wooriga/ejm_task/EjmTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.wooriga.ejm_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjmTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
